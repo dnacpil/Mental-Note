38fa3e1569ab3f381df86b883705b607c16aa6fb
dysiedaisy <dysielnacpil@gmail.com>
2023-11-11 21:01:14 +0000

    template files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MentalNote.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
