4666e2f0dfa55d1598547d4e160ecda56f5dbd45
dysiedaisy <dysielnacpil@gmail.com>
2023-12-24 10:52:46 +0000

    More changes on Identity pages

--- Controllers/MoodRatingController.cs
+++ Controllers/MoodRatingController.cs
@@ -18,17 +18,56 @@ public class MoodRatingController : Controller
         _db = context;
     }
 
-/* [HttpPost]
-public IActionResult SubmitMood(MoodRating moodRating)
-{
-    moodRating.OwnerId = userManager.GetUserId(User);
+    [HttpPost]
+    /* public IActionResult SubmitMood(MoodRating moodRating)
+    {
+        moodRating.OwnerId = userManager.GetUserId(User);
 
-    moodRating.RatingDate = DateTime.Now;
+        moodRating.RatingDate = DateTime.Now;
 
-    dbContext.MoodRatings.Add(moodRating);
-    dbContext.SaveChanges();
+        _dbContext.MoodRatings.Add(moodRating);
+        _dbContext.SaveChanges();
 
-    return RedirectToAction("Index");*/
+        return RedirectToAction("Index"); 
+    
 
+} */
+    public IActionResult SaveMoodRating(int rating)
+    {
+        // Assuming your Syncfusion component sends the selected mood as an integer.
+        // The integer could represent the index or any other value that maps to your mood levels.
 
+        // Map the rating value to a corresponding emoji or mood level as needed.
+        string moodEmoji = GetMoodEmoji(rating);
+
+        // Create a new MoodRating instance and populate its properties.
+        MoodRating moodRating = new MoodRating
+        {
+
+            RatingDate = DateTime.Now,
+            Rating = rating,
+            MoodNote = moodEmoji
+        };
+
+        return RedirectToAction(nameof(Index));
+
+    }
+    private string GetMoodEmoji(int rating)
+    {
+        // Map the rating to the corresponding emoji or mood level based on your logic.
+        // You can use a switch statement, if-else, or any other mapping mechanism.
+        switch (rating)
+        {
+            case 0:
+                return "&#128545"; // Angry emoji
+            case 1:
+                return "&#128577"; // Disagree emoji
+            case 2:
+                return "&#128528"; // Neutral emoji
+            case 3:
+                return "&#128578"; // Agree emoji
+            default:
+                return "😃"; // Happy emoji
+        }
+    }
 }
